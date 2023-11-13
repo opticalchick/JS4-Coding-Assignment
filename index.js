18627603ef806b6866304616a7c8877db73e4d3e
Becky Van Vickle <opticalchick@yahoo.com>
2023-11-13 14:09:36 -0600

    added gitignore file

---
+++ index.js
@@ -0,0 +1,18 @@
+//Task 1 
+// Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+// Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+// Do not use numbers to reference the last element, find it programmatically.
+// ages[7] - ages[0] is not allowed!
+// Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+// Use a loop to iterate through the array and calculate the average age.
+
+let ages = [3, 9, 23, 64, 2, 8, 28, 93];
+//let minusAge = ages[ages.length - 1] - ages[0];
+//console.log(minusAge);
+
+ages.push(17);
+let minusAge = ages[ages.length - 1] - ages[0];
+console.log(minusAge);
+
+
+
